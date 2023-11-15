3bd3b90061740620a75144cef25e9a7eea4da1f5
BellDHamster <150895665+BellDHamster@users.noreply.github.com>
2023-11-15 19:04:23 +0700

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '2023-15-11';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'https://i.imgur.com/';
+const imageRoot = 'src/assets/chars/';
